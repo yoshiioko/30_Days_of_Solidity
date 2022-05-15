4b4d0803616dec41f1c8eb47c4c6ab026ce70216
yoshiioko <acardoza@pdx.edu>
2022-05-15 09:01:00 -0700

    Add: migration and test file for DayOnePiggyBank

---
+++ migrations/2_deploy_DayOnePiggyBank.js
@@ -0,0 +1,5 @@
+const DayOnePiggyBank = artifacts.require("DayOnePiggyBank");
+
+module.exports = function (deployer) {
+  deployer.deploy(DayOnePiggyBank);
+};
---
+++ test/day_one_piggy_bank.js
@@ -0,0 +1,13 @@
+const DayOnePiggyBank = artifacts.require("DayOnePiggyBank");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("DayOnePiggyBank", function (/* accounts */) {
+  it("should assert true", async function () {
+    await DayOnePiggyBank.deployed();
+    return assert.isTrue(true);
+  });
+});
