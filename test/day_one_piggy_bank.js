43ddd0e9f4f095682b87cd2f7a3411b35c432281
yoshiioko <acardoza@pdx.edu>
2022-05-15 19:09:50 -0700

    Update: removing comments

--- test/day_one_piggy_bank.js
+++ test/day_one_piggy_bank.js
@@ -3,11 +3,6 @@ const {
   expectEvent, // Assertions for emitted events
 } = require("@openzeppelin/test-helpers");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
 contract("DayOnePiggyBank", function (accounts) {
   const [owner, nonOwner] = accounts;
 
