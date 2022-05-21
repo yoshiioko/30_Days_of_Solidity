9232d4736c5dca0df5addf257701cd56954c2cd3
yoshiioko <acardoza@pdx.edu>
2022-05-21 13:24:05 -0700

    removing comments

--- test/day_three_simple_calculator.js
+++ test/day_three_simple_calculator.js
@@ -1,10 +1,5 @@
 const DayThreeSimpleCalculator = artifacts.require("DayThreeSimpleCalculator");
 
-/*
- * uncomment accounts to access the test accounts made available by the
- * Ethereum client
- * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
- */
 contract("DayThreeSimpleCalculator", function (/* accounts */) {
   beforeEach(async function () {
     this.contract = await DayThreeSimpleCalculator.new();
